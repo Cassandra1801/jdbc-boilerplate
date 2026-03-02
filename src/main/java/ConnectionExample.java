6ac38e0b3d50ae7361eb675a545899855a4dd512
dean <nicklasfrederiksen@gmail.com>
2026-03-02 11:28:57 +0100

    Changed to pokemon data

--- src/main/java/ConnectionExample.java
+++ src/main/java/ConnectionExample.java
@@ -4,7 +4,7 @@ public class ConnectionExample {
     public static void main(String[] args) {
         try{
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/[YOUR DATABASE NAME]","[YOUR USERNAME]","[YOUR PASSWORD]");
-            PreparedStatement psts = conn.prepareStatement("SELECT * from employees");
+            PreparedStatement psts = conn.prepareStatement("SELECT * FROM pokemon");
             ResultSet resultSet = psts.executeQuery();
             while(resultSet.next()){
                 System.out.println(resultSet.getString(2));
